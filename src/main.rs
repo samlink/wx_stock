62042f5ea58e63451384e80388d27749e6f6ed37
sam <samlink@126.com>
2020-08-26 09:59:50 +0800

    change

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,17 @@
 use actix_files as fs;
 use actix_identity::{CookieIdentityPolicy, IdentityService};
 use actix_web::{web, App, HttpServer};
+use deadpool_postgres::{Config, ManagerConfig, RecyclingMethod};
+use rand::Rng;
 use tokio_postgres::NoTls;
 
-// use r2d2::Pool;
-// use r2d2_postgres::{PostgresConnectionManager, TlsMode};
-// use r2d2_postgres::PostgresConnectionManager;
-use deadpool_postgres::{Config, Manager, ManagerConfig, Pool, RecyclingMethod};
-use rand::Rng;
-
-mod home;
+mod html;
 mod service;
 mod user;
 mod useraes;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    // let manager = PostgresConnectionManager::new(
-    //     "postgres://postgres:sam197298@localhost:5432/stock"
-    //         .parse()
-    //         .unwrap(),
-    //     NoTls,
-    // );
-
-    // let manager = PostgresConnectionManager::new(
-    //     "postgres://postgres:sam197298@localhost:5432/stock",
-    //     TlsMode::None,
-    // )
-    // .unwrap();
-
-    // let pool = Pool::new(manager).unwrap();
-
-    // let mut cfg = tokio_postgres::Config::new();
-    // cfg.host("127.0.0.1");
-    // cfg.user("postgres");
-    // cfg.password("sam197298");
-    // cfg.dbname("stock");
-    // let mgr = Manager::new(cfg, tokio_postgres::NoTls);
-    // let pool = Pool::new(mgr, 6);
-
     let mut cfg = Config::new();
     cfg.host = Some("127.0.0.1".to_string());
     cfg.user = Some("postgres".to_string());
@@ -61,8 +34,8 @@ async fn main() -> std::io::Result<()> {
                     .name("auth-sales")
                     .secure(false),
             ))
-            .service(home::index)
-            .service(home::login)
+            .service(html::index)
+            .service(html::login)
             .service(user::login)
             .service(user::logon)
             .service(web::resource("static/{name}").to(service::serve_static))
--- src/user.rs
+++ src/user.rs
@@ -4,9 +4,6 @@ use actix_identity::Identity;
 use actix_web::{get, post, web, HttpResponse};
 use crypto::digest::Digest;
 use crypto::md5::Md5;
-// use r2d2::Pool;
-// use r2d2_postgres::PostgresConnectionManager;
-// use tokio_postgres::{NoTls, Error};
 use deadpool_postgres::{Client, Pool};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
@@ -142,10 +139,13 @@ pub async fn logon(db: web::Data<Pool>, user: web::Json<User>, id: Identity) -> 
     } else {
         let md5_pass = md5(user.password.clone(), SALT);
 
-        &conn.execute(
-            r#"INSERT INTO users (name, password) VALUES($1,$2)"#,
-            &[&user.name, &md5_pass],
-        );
+        &conn
+            .execute(
+                r#"INSERT INTO users (name, password) VALUES($1,$2)"#,
+                &[&user.name, &md5_pass],
+            )
+            .await
+            .unwrap();
 
         id.remember(user.name.clone());
         HttpResponse::Ok().json(user.name.clone())
@@ -158,18 +158,23 @@ pub async fn login(db: web::Data<Pool>, user: web::Json<User>, id: Identity) -> 
     let conn: Client = db.get().await.unwrap();
     static MAX_FAILED: i32 = 6;
 
-    let row = &conn
-        .query_one(r#"SELECT failed FROM users Where name=$1"#, &[&user.name])
+    let rows = &conn
+        .query(r#"SELECT failed FROM users Where name=$1"#, &[&user.name])
         .await
         .unwrap();
 
-    if let Some(one_row) = Some(row) {
-        let mut failed: i32 = one_row.get("failed");
+    if !rows.is_empty() {
+        let mut failed = -1i32;
+        for row in rows {
+            failed = row.get("failed");
+        }
 
         if failed >= MAX_FAILED {
             HttpResponse::Ok().json(MAX_FAILED)
         } else {
             let md5_pass = md5(user.password.clone(), SALT);
+            let mut name = "".to_owned();
+
             let rows = &conn
                 .query(
                     r#"SELECT name FROM users Where name=$1 AND password=$2"#,
@@ -177,25 +182,29 @@ pub async fn login(db: web::Data<Pool>, user: web::Json<User>, id: Identity) -> 
                 )
                 .await
                 .unwrap();
-
             if !rows.is_empty() {
-                let mut name = "".to_owned();
                 for row in rows {
                     name = row.get("name");
                 }
 
                 let encrypted_data = encrypt(name.as_bytes(), KEY, IV).ok().unwrap(); //将 name 用 aes 加密保存到前端存储
-                &conn.execute(r#"UPDATE users SET failed=0 WHERE name=$1"#, &[&user.name]);
+                &conn
+                    .execute(r#"UPDATE users SET failed=0 WHERE name=$1"#, &[&user.name])
+                    .await
+                    .unwrap();
 
                 id.remember(name);
 
                 HttpResponse::Ok().json(encrypted_data)
             } else {
                 failed += 1;
-                &conn.execute(
-                    r#"UPDATE users SET failed=$1 WHERE name=$2"#,
-                    &[&failed, &user.name],
-                );
+                &conn
+                    .execute(
+                        r#"UPDATE users SET failed=$1 WHERE name=$2"#,
+                        &[&failed, &user.name],
+                    )
+                    .await
+                    .unwrap();
 
                 HttpResponse::Ok().json(MAX_FAILED - failed)
             }
@@ -211,138 +220,143 @@ pub fn logout(id: Identity) -> HttpResponse {
     HttpResponse::Found().json(1)
 }
 
-// 更改用户密码
-// pub fn change_pass(
-//     db: web::Data<Pool<PostgresConnectionManager<tokio_postgres::tls::NoTls>>>,
-//     user: web::Json<ChangePass>,
-//     id: Identity,
-// ) -> HttpResponse {
-//     let user_name = id.identity().unwrap_or("0".to_owned());
+///更改用户密码
+pub async fn change_pass(
+    db: web::Data<Pool>,
+    user: web::Json<ChangePass>,
+    id: Identity,
+) -> HttpResponse {
+    let user_name = id.identity().unwrap_or("0".to_owned());
 
-//     if user_name == "guest" || user_name == "0" {
-//         return HttpResponse::Ok().json(0);
-//     }
+    if user_name == "guest" || user_name == "0" {
+        return HttpResponse::Ok().json(0);
+    }
 
-//     let mut conn = db.get().unwrap();
-//     let salt_pass = md5(user.old_pass.clone(), SALT);
+    let conn = db.get().await.unwrap();
+    let salt_pass = md5(user.old_pass.clone(), SALT);
 
-//     let rows = &conn
-//         .query(
-//             r#"SELECT name FROM users Where name=$1 AND password=$2"#,
-//             &[&user_name, &salt_pass],
-//         )
-//         .unwrap();
+    let rows = &conn
+        .query(
+            r#"SELECT name FROM users Where name=$1 AND password=$2"#,
+            &[&user_name, &salt_pass],
+        )
+        .await
+        .unwrap();
 
-//     if rows.is_empty() {
-//         HttpResponse::Ok().json(0)
-//     } else {
-//         let new_pass = md5(user.new_pass.clone(), SALT);
-//         &conn.execute(
-//             r#"UPDATE users SET password=$1 WHERE name=$2"#,
-//             &[&new_pass, &user_name],
-//         );
+    if rows.is_empty() {
+        HttpResponse::Ok().json(0)
+    } else {
+        let new_pass = md5(user.new_pass.clone(), SALT);
+        &conn
+            .execute(
+                r#"UPDATE users SET password=$1 WHERE name=$2"#,
+                &[&new_pass, &user_name],
+            )
+            .await
+            .unwrap();
 
-//         HttpResponse::Ok().json(1)
-//     }
-// }
+        HttpResponse::Ok().json(1)
+    }
+}
 
-// ///设置手机号
-// pub fn phone_number(
-//     db: web::Data<Pool<PostgresConnectionManager<tokio_postgres::tls::NoTls>>>,
-//     user: web::Json<Phone>,
-//     id: Identity,
-// ) -> HttpResponse {
-//     let user_name = id.identity().unwrap_or("0".to_owned());
-//     let mut conn = db.get().unwrap();
+///设置手机号
+pub async fn phone_number(
+    db: web::Data<Pool>,
+    user: web::Json<Phone>,
+    id: Identity,
+) -> HttpResponse {
+    let user_name = id.identity().unwrap_or("0".to_owned());
+    let conn = db.get().await.unwrap();
 
-//     if user_name != "guest" && user_name != "0" {
-//         &conn.execute(
-//             r#"UPDATE users SET phone=$1 WHERE name=$2"#,
-//             &[&user.phone_number, &user_name],
-//         );
-//     }
+    if user_name != "guest" && user_name != "0" {
+        &conn
+            .execute(
+                r#"UPDATE users SET phone=$1 WHERE name=$2"#,
+                &[&user.phone_number, &user_name],
+            )
+            .await
+            .unwrap();
+    }
 
-//     HttpResponse::Ok().json(1)
-// }
+    HttpResponse::Ok().json(1)
+}
 
-// ///设置主题
-// pub fn change_theme(
-//     db: web::Data<Pool<PostgresConnectionManager<tokio_postgres::tls::NoTls>>>,
-//     theme: web::Json<Theme>,
-//     id: Identity,
-// ) -> HttpResponse {
-//     let user_name = id.identity().unwrap();
-//     let mut conn = db.get().unwrap();
-//     &conn.execute(
-//         r#"UPDATE users SET theme=$1 WHERE name=$2"#,
-//         &[&theme.name, &user_name],
-//     );
+///设置主题
+pub async fn change_theme(
+    db: web::Data<Pool>,
+    theme: web::Json<Theme>,
+    id: Identity,
+) -> HttpResponse {
+    let user_name = id.identity().unwrap();
+    let conn = db.get().await.unwrap();
+    &conn
+        .execute(
+            r#"UPDATE users SET theme=$1 WHERE name=$2"#,
+            &[&theme.name, &user_name],
+        )
+        .await
+        .unwrap();
 
-//     HttpResponse::Ok().json(1)
-// }
+    HttpResponse::Ok().json(1)
+}
 
-// ///找回密码
-// pub fn forget_pass(
-//     db: web::Data<Pool<PostgresConnectionManager<tokio_postgres::tls::NoTls>>>,
-//     user: web::Json<User>,
-// ) -> HttpResponse {
-//     let mut conn = db.get().unwrap();
-//     static MAX_PASS: i32 = 6;
+///找回密码
+pub async fn forget_pass(db: web::Data<Pool>, user: web::Json<User>) -> HttpResponse {
+    let conn = db.get().await.unwrap();
+    static MAX_PASS: i32 = 6;
 
-//     let row = &conn
-//         .query_one(
-//             r#"SELECT name, phone, get_pass FROM users Where name=$1"#,
-//             &[&user.name],
-//         )
-//         .unwrap();
+    let row = &conn
+        .query_one(
+            r#"SELECT name, phone, get_pass FROM users Where name=$1"#,
+            &[&user.name],
+        )
+        .await
+        .unwrap();
 
-//     if let Some(one_row) = Some(row) {
-//         let user_name: String = one_row.get("name");
-//         let phone: String = match one_row.get("phone") {
-//             Some(phone) => phone,
-//             None => "".to_owned(),
-//         };
-//         let mut get_pass: i32 = one_row.get("get_pass");
+    let user_name: String = row.get("name");
+    let phone: String = match row.get("phone") {
+        Some(phone) => phone,
+        None => "".to_owned(),
+    };
+    let mut get_pass: i32 = row.get("get_pass");
 
-//         if phone == "" {
-//             HttpResponse::Ok().json(-2)
-//         } else if get_pass >= MAX_PASS {
-//             HttpResponse::Ok().json(-3)
-//         } else {
-//             let words = "abcdefghijklmnpqrstuvwxyz123456789";
-//             let mut new_pass = "".to_owned();
+    if phone == "" {
+        HttpResponse::Ok().json(-2)
+    } else if get_pass >= MAX_PASS {
+        HttpResponse::Ok().json(-3)
+    } else {
+        let words = "abcdefghijklmnpqrstuvwxyz123456789";
+        let mut new_pass = "".to_owned();
 
-//             for _n in 1..5 {
-//                 let num = rand::thread_rng().gen_range(0, 34);
-//                 new_pass.push_str(&words[num..num + 1]);
-//             }
+        for _n in 1..5 {
+            let num = rand::thread_rng().gen_range(0, 34);
+            new_pass.push_str(&words[num..num + 1]);
+        }
 
-//             let salt_pass = md5(new_pass.clone(), SALT);
-//             get_pass += 1;
+        let salt_pass = md5(new_pass.clone(), SALT);
+        get_pass += 1;
 
-//             &conn.execute(
-//                 r#"UPDATE users SET password=$1, get_pass=$3 WHERE name=$2"#,
-//                 &[&salt_pass, &user_name, &get_pass],
-//             );
+        &conn
+            .execute(
+                r#"UPDATE users SET password=$1, get_pass=$3 WHERE name=$2"#,
+                &[&salt_pass, &user_name, &get_pass],
+            )
+            .await
+            .unwrap();
 
-//             let send = SendMessage {
-//                 apikey: "011a9a2b503aa72978bd77ec4577a675".to_owned(),
-//                 mobile: phone,
-//                 text: "【鼎传码行】密码已重置，新的密码为：".to_owned()
-//                     + &new_pass
-//                     + " ，请及时操作",
-//             };
+        let send = SendMessage {
+            apikey: "011a9a2b503aa72978bd77ec4577a675".to_owned(),
+            mobile: phone,
+            text: "【鼎传码行】密码已重置，新的密码为：".to_owned() + &new_pass + " ，请及时操作",
+        };
 
-//             let client = reqwest::Client::new();
-//             let _res = client
-//                 .post("https://sms.yunpian.com/v2/sms/single_send.json")
-//                 .form(&send)
-//                 .send()
-//                 .unwrap();
+        let client = reqwest::Client::new();
+        let _res = client
+            .post("https://sms.yunpian.com/v2/sms/single_send.json")
+            .form(&send)
+            .send()
+            .unwrap();
 
-//             HttpResponse::Ok().json(MAX_PASS - get_pass)
-//         }
-//     } else {
-//         HttpResponse::Ok().json(-1)
-//     }
-// }
+        HttpResponse::Ok().json(MAX_PASS - get_pass)
+    }
+}
