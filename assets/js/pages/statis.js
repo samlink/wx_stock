c9a0fd825ea1ce5f08e56ec999e1428fe5c5d764
Sam Zhou <samlink@126.com>
2021-01-17 14:39:45 +0800

    change

--- assets/js/pages/statis.js
+++ assets/js/pages/statis.js
@@ -9,6 +9,7 @@ let date2 = new Intl.DateTimeFormat('fr-CA').format(new Date())
 
 laydate.render({
     elem: '#search-date',
+    // type: 'year',
     type: 'month',
     range: true,
     theme: 'molv',
@@ -30,8 +31,17 @@ var myChart = new Chart(ctx, {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '销售额',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            data: [12, 9, 23, 5, 2, 3],
+            backgroundColor:
+            [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            // backgroundColor: 'rgba(54, 162, 235, 0.5)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1
         }]
