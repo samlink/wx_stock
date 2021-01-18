827df3563c4a4c0541479db8e246f1163135a72e
Sam Zhou <samlink@126.com>
2021-01-18 20:28:50 +0800

    change

--- assets/js/pages/statis.js
+++ assets/js/pages/statis.js
@@ -2,6 +2,7 @@ import { notifier } from '../parts/notifier.mjs';
 import { SPLITER } from '../parts/tools.mjs';
 
 var ctx = document.getElementById('myChart').getContext('2d');
+
 var char_data = {
     type: 'bar',
     data: {
@@ -27,19 +28,85 @@ var char_data = {
 
 var myChart;
 
+let da = new Date(Date.now());
+let da2 = da.setFullYear(da.getFullYear() - 1);
+let date1 = new Intl.DateTimeFormat('fr-CA').format(da2);
+let date2 = new Intl.DateTimeFormat('fr-CA').format(new Date());
+let value_month = `${date1.substring(0, 7)} - ${date2.substring(0, 7)}`;
+
+laydate.render({
+    elem: '#search-date-month',
+    type: "month",
+    range: true,
+    value: value_month,
+    theme: 'molv',
+});
+
+da = new Date(Date.now());
+da2 = da.setFullYear(da.getFullYear() - 10);
+date1 = new Intl.DateTimeFormat('fr-CA').format(da2);
+date2 = new Intl.DateTimeFormat('fr-CA').format(new Date());
+
+let value_year = `${date1.substring(0, 4)} - ${date2.substring(0, 4)}`;
+
+laydate.render({
+    elem: '#search-date-year',
+    type: "year",
+    range: true,
+    value: value_year,
+    theme: 'molv',
+});
+
 let statis_cate_s = localStorage.getItem("statis_cate");
 let chart_cate_s = localStorage.getItem("chart_cate");
 
 let statis_cate = statis_cate_s ? statis_cate_s : "按月";
 let chart_cate = chart_cate_s ? chart_cate_s : "柱状图";
 
+document.querySelector('#chart-cate').value = chart_cate;
+
 if (statis_cate == "按月") {
-    set_cate_date("按月");
+    document.querySelector('#statis-cate').value = "按月";
+    document.querySelector('#search-date-month').style.display = "inline-block";
+    document.querySelector('#search-date-year').style.display = "none";
+    document.querySelector('#search-date-week').style.display = "none";
+
+    let date = value_month.split(' - ');
+
+    let data = {
+        statis_cate: statis_cate,
+        chart_cate: chart_cate,
+        date1: date[0],
+        date2: date[1],
+    };
+
+    set_chart(data);
 }
 else if (statis_cate == "按年") {
-    set_cate_date("按年");
+    document.querySelector('#statis-cate').value = "按年";
+    document.querySelector('#search-date-month').style.display = "none";
+    document.querySelector('#search-date-year').style.display = "inline-block";
+    document.querySelector('#search-date-week').style.display = "none";
+
+    let date = value_year.split(' - ');
+
+    let data = {
+        statis_cate: statis_cate,
+        chart_cate: chart_cate,
+        date1: date[0],
+        date2: date[1],
+    };
+
+    set_chart(data);
 }
 else {
+    document.querySelector('#statis-cate').value = "按周";
+    document.querySelector('#search-date-month').style.display = "none";
+    document.querySelector('#search-date-year').style.display = "none";
+    document.querySelector('#search-date-week').style.display = "inline-block";
+
+    document.querySelector('#search-date-week').value = 24;
+
     let data = {
         statis_cate: statis_cate,
         chart_cate: chart_cate,
@@ -50,161 +117,76 @@ else {
     set_chart(data);
 }
 
-function get_date(cate) {
-    let n1 = 1, n2 = 7, type = "month";
-    if (cate == "按年") {
-        n1 = 10;
-        n2 = 4;
-        type = "year";
-    }
-
-    let da = new Date(Date.now());
-    let da2 = da.setFullYear(da.getFullYear() - n1);
-    let date1 = new Intl.DateTimeFormat('fr-CA').format(da2);
-    let date2 = new Intl.DateTimeFormat('fr-CA').format(new Date());
-    let value = `${date1.substring(0, n2)} - ${date2.substring(0, n2)}`
-
-    laydate.render({
-        elem: '#search-date',
-        type: type,
-        range: true,
-        value: value,
-        theme: 'molv',
-    });
-
-    return value;
-}
-
-function set_cate_date(cate) {
-    let date_s = get_date(cate).split(" - ");
-
-    let data = {
-        statis_cate: statis_cate,
-        chart_cate: chart_cate,
-        date1: date_s[0],
-        date2: date_s[1],
-    };
-
-    set_chart(data);
-}
-
 document.querySelector('#statis-cate').addEventListener('change', function () {
     if (this.value == "按月") {
-        get_date("按月");
+        document.querySelector('#search-date-month').style.display = "inline-block";
+        document.querySelector('#search-date-year').style.display = "none";
+        document.querySelector('#search-date-week').style.display = "none";
+        localStorage.setItem("statis_cate", "按月");
+
     }
     else if (this.value == "按年") {
-        get_date("按年");
+        document.querySelector('#search-date-month').style.display = "none";
+        document.querySelector('#search-date-year').style.display = "inline-block";
+        document.querySelector('#search-date-week').style.display = "none";
+        localStorage.setItem("statis_cate", "按年");
     }
     else {
-        document.querySelector('#search-date').value = "24";
+        document.querySelector('#search-date-month').style.display = "none";
+        document.querySelector('#search-date-year').style.display = "none";
+        document.querySelector('#search-date-week').style.display = "inline-block";
+        localStorage.setItem("statis_cate", "按周");
     }
 });
 
+document.querySelector('#chart-cate').addEventListener('change', function () {
+    localStorage.setItem("chart_cate", this.value);
+});
+
+
+char_data.data.labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+char_data.data.datasets[0].data = [12, 9, 23, 5, 2, 3];
+
+myChart = new Chart(ctx, char_data);
 
 function set_chart(data) {
-    // fetch("/fetch_statis", {
-    //     method: 'post',
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    // })
-    //     .then(response => response.json())
-    //     .then(content => {
-    //         if (content != -1) {
-    //             document.querySelector('.customer-name').textContent = customer;
-    //             if (flag == 0) {
-    //                 flag = 1;
-    //                 let div_list = document.querySelector('.name-list');
-    //                 let names = "";
+    fetch("/fetch_statis", {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content != -1) {
+                // let row = "<tr>";
+                // for (let d of debt4) {
+                //     d = d == 0 ? "" : d;
+                //     row += `<td>${d}</td>`;
+                // }
+                // row += "</tr>";
+                // rows += row;
 
-    //                 for (let name of content[0]) {
-    //                     names += `<p>${name}</p>`;
-    //                 }
+                // s1 = debt3[1] * 1 + debt4[1] * 1;
+                // s2 = debt3[2] * 1 - debt4[2] * 1;
+                // s3 = debt3[3] * 1 - debt4[3] * 1;
+                // s4 = debt3[4] * 1 - debt4[4] * 1;
+                // s5 = debt3[5] * 1 - debt4[5] * 1;
 
-    //                 div_list.innerHTML = names;
+                // rows += `<tr><td>小计</td><td>${s1 == 0 ? "" : s1}</td><td>${s2 == 0 ? "" : s2.toFixed(p)}</td><td>${s3 == 0 ? "" : s3.toFixed(p)}</td>
+                //             <td>${s4 == 0 ? "" : s4.toFixed(p)}</td><td>${s5 == 0 ? "" : s5.toFixed(p)}</td></tr>`;
 
-    //                 let ps = div_list.querySelectorAll("p");
-    //                 for (let p of ps) {
-    //                     p.addEventListener("click", function () {
-    //                         for (let p1 of ps) {
-    //                             p1.classList.remove('focus');
-    //                         }
-    //                         this.classList.add('focus');
-    //                         document.querySelector("#search-customer").value = p.textContent;
-    //                         search_button.click();
+                // document.querySelector('.table-container tbody').innerHTML = rows;
 
-    //                     });
-    //                 }
-    //             }
+                char_data.data.labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+                char_data.data.datasets[0].data = [12, 9, 23, 5, 2, 3];
 
-    //             if (content[1].length > 0) {
-    //                 let rows = "";
-    //                 let debt1 = content[1][0].split(SPLITER);
-    //                 let debt2 = content[1][1].split(SPLITER);
-    //                 let debt3 = content[1][2].split(SPLITER);
-    //                 let debt4 = content[1][3].split(SPLITER);
-
-    //                 let s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0;
-
-    //                 let row = "<tr>";
-    //                 for (let d of debt1) {
-    //                     d = d == 0 ? "" : d;
-    //                     row += `<td>${d}</td>`;
-    //                 }
-    //                 row += "</tr>";
-    //                 rows += row;
-
-    //                 row = "<tr>";
-    //                 for (let d of debt2) {
-    //                     d = d == 0 ? "" : d;
-    //                     row += `<td>${d}</td>`;
-    //                 }
-    //                 row += "</tr>";
-    //                 rows += row;
-
-    //                 s1 = debt1[1] * 1 + debt2[1] * 1;
-    //                 s2 = debt1[2] * 1 - debt2[2] * 1;
-    //                 s3 = debt1[3] * 1 - debt2[3] * 1;
-    //                 s4 = debt1[4] * 1 - debt2[4] * 1;
-    //                 s5 = debt1[5] * 1 - debt2[5] * 1;
-
-    //                 rows += `<tr><td>小计</td><td>${s1 == 0 ? "" : s1}</td><td>${s2 == 0 ? "" : s2.toFixed(p)}</td><td>${s3 == 0 ? "" : s3.toFixed(p)}</td>
-    //                 <td>${s4 == 0 ? "" : s4.toFixed(p)}</td><td>${s5 == 0 ? "" : s5.toFixed(p)}</td></tr>`;
-
-    //                 s1 = 0; s2 = 0; s3 = 0; s4 = 0; s5 = 0;
-
-    //                 row = "<tr>";
-    //                 for (let d of debt3) {
-    //                     d = d == 0 ? "" : d;
-    //                     row += `<td>${d}</td>`;
-    //                 }
-    //                 row += "</tr>";
-    //                 rows += row;
-
-    //                 row = "<tr>";
-    //                 for (let d of debt4) {
-    //                     d = d == 0 ? "" : d;
-    //                     row += `<td>${d}</td>`;
-    //                 }
-    //                 row += "</tr>";
-    //                 rows += row;
-
-    //                 s1 = debt3[1] * 1 + debt4[1] * 1;
-    //                 s2 = debt3[2] * 1 - debt4[2] * 1;
-    //                 s3 = debt3[3] * 1 - debt4[3] * 1;
-    //                 s4 = debt3[4] * 1 - debt4[4] * 1;
-    //                 s5 = debt3[5] * 1 - debt4[5] * 1;
-
-    //                 rows += `<tr><td>小计</td><td>${s1 == 0 ? "" : s1}</td><td>${s2 == 0 ? "" : s2.toFixed(p)}</td><td>${s3 == 0 ? "" : s3.toFixed(p)}</td>
-    //                         <td>${s4 == 0 ? "" : s4.toFixed(p)}</td><td>${s5 == 0 ? "" : s5.toFixed(p)}</td></tr>`;
-
-    //                 document.querySelector('.table-container tbody').innerHTML = rows;
-    //             }
-    //         }
-    //         else {
-    //             notifier.show('无操作权限', 'danger');
-    //         }
-    //     });
+                myChart = new Chart(ctx, char_data);
+            }
+            else {
+                notifier.show('无操作权限', 'danger');
+            }
+        });
 }
 
