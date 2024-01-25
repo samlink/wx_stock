114764510e03ccbfe3c36a80fe3f88c6487e9ac3
Sam Zhou <samlink@126.com>
2024-01-25 10:38:14 +0800

    change

---
+++ assets/js_max/pages/fieldset.js
@@ -0,0 +1,321 @@
+import {notifier} from '../parts/notifier.mjs';
+import {regReal, getHeight} from '../parts/tools.mjs';
+
+let global = {
+    drag_id: "",
+    drag_tr: "",
+    edit: 0,
+    edit2: 0,
+}
+
+let sumit_button = document.querySelector('#sumit-button');
+let sumit_button2 = document.querySelector('#sumit-button2');
+sumit_button.disabled = true;
+sumit_button2.disabled = true;
+
+//显示表格数据 ---------------------------------------
+var table_name = {};
+
+document.querySelector('#table-choose').addEventListener('change', function () {
+    table_name.name = this.value;
+    document.querySelector('#choose-info').classList.add('hide');
+    fetch_data(table_name);
+    fetch_data2(table_name);
+});
+
+function row_fn(tr) {
+    let s1 = "";
+    let s2 = "";
+
+    if (tr.ctr_type == "普通输入") {
+        s1 = "selected";
+    } else if (tr.ctr_type == "下拉列表") {
+        s2 = "selected";
+    }
+
+    let select = tr.data_type != "布尔" ? `<select class='select-sm'><option value="普通输入" ${s1}>普通输入</option>
+                <option value="下拉列表" ${s2}>下拉列表</option></select></td>` :
+        `<select class='select-sm'><option value="二值选一" selected>二值选一</option></select></td>`;
+
+    let read_only = tr.ctr_type == "普通输入" ? "disabled" : "";
+
+
+    let checked1 = tr.is_show ? "checked" : "";
+    let checked2 = tr.is_use ? "checked" : "";
+    let disabled = tr.all_edit ? "" : "disabled";
+    let style = tr.all_edit ? "" : "style='background: lightgrey;border: none;'";
+
+    return `<tr draggable="true"><td class='hide'>${tr.id}</td><td width=6%>${tr.num}</td><td width=15%>${tr.field_name}</td>
+            <td width=10%>${tr.data_type}</td><td width=15%><input class='form-control input-sm' type="text" value=${tr.show_name}></td>
+            <td width=8%><input class='form-control input-sm' type="text" value=${tr.show_width}></td>
+            <td width=15%>${select}</td>
+            <td width=20%><input class='form-control input-sm' type="text" ${read_only} value=${tr.option_value}></td>
+            <td width=10%><input class='form-control input-sm' type="text" ${read_only} value=${tr.default_value}></td>
+            <td width=8%><label class="check-radio"><input type="checkbox" ${checked2} ${disabled}>
+            <span class="checkmark" ${style}></span></td>
+            <td width=8%><label class="check-radio"><input type="checkbox" ${checked1} ${disabled}>
+            <span class="checkmark" ${style}></span></td></tr>`;
+}
+
+function blank_row_fn() {
+    return `<tr><td width=6%></td><td width=15%></td><td width=10%></td><td width=15%></td><td width=8%></td>
+            <td width=15%></td><td width=20%></td><td width=10%></td><td width=8%></td><td width=8%></td></tr>`;
+}
+
+function fetch_data(data) {
+    fetch(`/fetch_fields`, {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content != -1) {
+                let rows = "";
+                let count = 0;
+                for (let tr of content[0]) {
+                    rows += row_fn(tr);
+                    count++;
+                }
+
+                //多输入两个空行，便于拖拽操作
+                for (let i = 0; i < 2; i++) {
+                    rows += blank_row_fn();
+                }
+
+                let table_body = document.querySelector('.table-product tbody');
+                table_body.innerHTML = rows;
+                document.querySelector('#total-records').textContent = content[1];
+
+                //拖拽功能
+                row_drag(table_body, sumit_button);
+
+                //编辑时离开提醒
+                let all_input = document.querySelectorAll('.table-product tbody input');
+                for (let input of all_input) {
+                    input.addEventListener('input', () => {
+                        sumit_button.disabled = false;
+                        global.edit = 1;
+                    });
+                }
+
+                let all_select = document.querySelectorAll('.table-product tbody select');
+                for (let select of all_select) {
+                    select.addEventListener('change', function () {
+                        document.querySelector('#sumit-button').disabled = false;
+                        global.edit = 1;
+                        if (this.value != "普通输入") {
+                            this.parentNode.nextElementSibling.querySelector('input').disabled = false;
+                            this.parentNode.nextElementSibling.querySelector('input').focus();
+                        } else {
+                            this.parentNode.nextElementSibling.querySelector('input').disabled = true;
+                        }
+                    });
+                }
+            } else {
+                alert("无此操作权限");
+            }
+        });
+}
+
+function fetch_data2(data) {
+    fetch(`/fetch_fields2`, {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content != -1) {
+                let rows = "";
+                let count = 0;
+                for (let tr of content[0]) {
+                    rows += row_fn2(tr);
+                    count++;
+                }
+
+                //多输入两个空行，便于拖拽操作
+                for (let i = 0; i < 2; i++) {
+                    rows += blank_row_fn2();
+                }
+
+                let table_body = document.querySelector('.table-inout tbody');
+                table_body.innerHTML = rows;
+                document.querySelector('#total-records2').textContent = content[1];
+
+                //拖拽功能
+                row_drag(table_body, sumit_button2);
+
+                //编辑时离开提醒
+                let all_input = document.querySelectorAll('.table-inout tbody input');
+                for (let input of all_input) {
+                    input.addEventListener('input', () => {
+                        sumit_button2.disabled = false;
+                        global.edit2 = 1;
+                    });
+                }
+            } else {
+                alert("无此操作权限");
+            }
+        });
+}
+
+function row_fn2(tr) {
+    let checked = tr.inout_show ? "checked" : "";
+    let disabled = tr.all_edit ? "" : "disabled";
+    let style = tr.all_edit ? "" : "style='background: lightgrey;border: none;'";
+
+    return `<tr draggable="true"><td class='hide'>${tr.id}</td>
+            <td width=20%>${tr.num}</td><td>${tr.show_name}</td>
+            <td width=20%><input class='form-control input-sm' type="text" value=${tr.inout_width}></td>            
+            <td width=20%><label class="check-radio"><input type="checkbox" ${checked} ${disabled}>
+            <span class="checkmark" ${style}></span></td></tr>`;
+}
+
+function blank_row_fn2() {
+    return `<tr><td width=20%></td><td></td><td width=20%></td><td width=20%></td></tr>`;
+}
+
+function row_drag(table_body, sumit_button, bz) {
+    for (let tr of table_body.children) {
+        tr.addEventListener('dragstart', function (e) {
+            e.stopPropagation();
+            global.drag_tr = e.target;
+        });
+
+        tr.addEventListener('dragover', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.style.cssText = "color: red; background-color: lightyellow; font-weight: 600;";
+
+        });
+
+        tr.addEventListener('dragleave', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.style.cssText = "";
+        });
+
+        tr.addEventListener('drop', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.style.cssText = "";
+            table_body.insertBefore(global.drag_tr, this);
+            sumit_button.disabled = false;
+            if (bz == 1) {
+                global.edit = 1;
+            } else {
+                global.edit2 = 1;
+            }
+        });
+    }
+}
+
+//提交按钮
+sumit_button.addEventListener('click', () => {
+    let data = [];
+    let order = 1;
+    let table_body = document.querySelector('.table-product tbody');
+
+    for (let tr of table_body.children) {
+        if (tr.querySelector('td:nth-child(1)').textContent != "") {
+            if (!regReal.test(Number(tr.querySelector('td:nth-child(6) input').value))) {
+                notifier.show('宽度输入有错误', 'danger');
+                return false;
+            }
+            let select = tr.querySelector('select');
+            if (select.value != "普通输入" && select.parentNode.nextElementSibling.querySelector('input').value == "") {
+                notifier.show('可选值还未输入', 'danger');
+                return false;
+            }
+        }
+    }
+
+    for (let tr of table_body.children) {
+        if (tr.querySelector('td:nth-child(1)').textContent != "") {
+            let tr_data = {
+                id: Number(tr.querySelector('td:nth-child(1)').textContent),
+                show_name: tr.querySelector('td:nth-child(5) input').value,
+                show_width: Number(tr.querySelector('td:nth-child(6) input').value),
+                ctr_type: tr.querySelector('td:nth-child(7) select').value,
+                option_value: tr.querySelector('td:nth-child(8) input').value,
+                default_value: tr.querySelector('td:nth-child(9) input').value,
+                is_use: tr.querySelector('td:nth-child(10) input').checked,
+                is_show: tr.querySelector('td:nth-child(11) input').checked,
+                show_order: order,
+            }
+            order++;
+            data.push(tr_data);
+        }
+    }
+
+    fetch(`/update_tableset`, {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content == 1) {
+                global.edit = 0;
+                fetch_data2(table_name);
+                document.querySelector('#sumit-button').disabled = true;
+                notifier.show('字段修改成功', 'success');
+            } else {
+                notifier.show('权限不够，操作失败', 'danger');
+            }
+        });
+});
+
+sumit_button2.addEventListener('click', () => {
+    let data = [];
+    let order = 1;
+    let table_body = document.querySelector('.table-inout tbody');
+
+    for (let tr of table_body.children) {
+        if (tr.querySelector('td:nth-child(1)').textContent != "") {
+            let tr_data = {
+                id: Number(tr.querySelector('td:nth-child(1)').textContent),
+                inout_width: Number(tr.querySelector('td:nth-child(4) input').value),
+                inout_show: tr.querySelector('td:nth-child(5) input').checked,
+                inout_order: order,
+
+            }
+            order++;
+            data.push(tr_data);
+        }
+    }
+
+    fetch(`/update_tableset2`, {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content == 1) {
+                global.edit2 = 0;
+                document.querySelector('#sumit-button2').disabled = true;
+                notifier.show('字段修改成功', 'success');
+            } else {
+                notifier.show('权限不够，操作失败', 'danger');
+            }
+        });
+});
+
+window.onbeforeunload = function (e) {
+    document.querySelector('#table-choose').value = "";
+    if (global.edit == 1 || global.edit2 == 1) {
+        var e = window.event || e;
+        e.returnValue = ("编辑未保存提醒");
+    }
+}
+
---
+++ assets/js_max/pages/stockoutitems.js
@@ -0,0 +1,108 @@
+import {table_init, fetch_table} from '../parts/table.mjs';
+import {notifier} from '../parts/notifier.mjs';
+import {AutoInput} from '../parts/autocomplete.mjs';
+import {SPLITER, getHeight, download_file} from '../parts/tools.mjs';
+import {set_date} from "../parts/service.mjs";
+
+let get_height = getHeight() - 133;
+let row_num = Math.floor(get_height / 33);
+
+//执行日期实例------------------------------------------------
+set_date();
+let date1 =  document.querySelector('#search-date1').value;
+let date2 = document.querySelector('#search-date2').value;
+
+//填充表格空行-------------------------------------------------
+let blank_rows = "";
+for (let i = 0; i < row_num; i++) {
+    blank_rows += blank_row_fn();
+}
+
+document.querySelector('.table-container tbody').innerHTML = blank_rows;
+
+//表格搜索----------------------------------------------------
+let init_data = {
+    container: '.table-container',
+    url: `/get_stockout_items`,
+    post_data: {
+        id: "",
+        name: '',
+        sort: "单号 DESC, 顺序",
+        rec: row_num,
+    },
+    edit: false,
+    header_names: {
+        "出库日期": "documents.日期",
+        "出库单号": "documents.单号",
+        "合同号": "documents.文本字段5",
+        "销售单号": "documents.文本字段6",
+        "公司名称": "documents.文本字段4",
+        "名称": "split_part(node_name,' ',2)",
+        "物料号": "products.文本字段1",
+        "材质": "split_part(node_name,' ',1)",
+        "规格": "规格型号",
+        "状态": "products.文本字段2",
+        "炉号": "products.文本字段4",
+        "长度": "products.整数字段1",
+        "数量": "数量",
+        "重量": "库存下限",
+        "备注": "pout_items.备注"
+    },
+
+    row_fn: row_fn,
+    blank_row_fn: blank_row_fn,
+};
+
+init_data.post_data.cate = `${date1}${SPLITER}${date2}`;
+
+table_init(init_data);
+fetch_table();
+
+//点击搜索按钮
+document.querySelector('#serach-button').addEventListener('click', function () {
+    let fields = document.querySelector('#search-fields').value;
+    let date1 = document.querySelector('#search-date1').value;
+    let date2 = document.querySelector('#search-date2').value;
+
+    init_data.post_data.name = fields;
+    init_data.post_data.cate = `${date1}${SPLITER}${date2}`;
+
+    // table_init(init_data);
+    fetch_table();
+});
+
+function row_fn(tr) {
+    let row = tr.split(SPLITER);
+    return `<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td><td>${row[3]}</td><td>${row[5]}</td>
+            <td>${row[4]}</td><td>${row[6]}</td><td>${row[7]}</td><td>${row[8]}</td><td>${row[9]}</td>
+            <td>${row[10]}</td><td>${row[11]}</td><td>${row[12]}</td><td>${row[13]}</td><td>${row[14]}</td>
+            <td>${row[15]}</td></tr>`;
+}
+
+function blank_row_fn() {
+    return `<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+            <td></td><td></td><td></td><td></td><td></td><td></td></tr>`;
+}
+
+document.querySelector('#data-out').addEventListener('click', ()=> {
+    let da1 = document.querySelector('#search-date1').value;
+    let da2 = document.querySelector('#search-date2').value;
+    let name = document.querySelector('#search-fields').value;
+    let data = `${da1}${SPLITER}${da2}${SPLITER}${name}`;
+    fetch(`/stockout_excel`, {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(content => {
+            if (content != -1) {
+                download_file(`/download/出库明细表.xlsx`);
+                notifier.show('成功导出至 Excel 文件', 'success');
+            } else {
+                notifier.show('权限不够，操作失败', 'danger');
+            }
+        });
+});
