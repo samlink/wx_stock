8aa900c1c0d938c1ea0e767e25ef3945e4585ffc
Sam Zhou <samlink@126.com>
2024-02-24 18:57:10 +0800

    change

--- put.sh
+++ put.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
-ftp_site=39.106.229.26
+site=39.106.229.26
 username=sam
-passwd=7298
 filename=sales
-PS3='Select a destination directory: '
 
 cd ./target/release
 
-do
-ftp -n $ftp_site<<EOF
-quote USER $username
+sftp $username@$site<<EOF
 quote PASS $passwd
 binary
-cd $path
 put $filename
 quit
 EOF
-break
-done
