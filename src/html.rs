07c5a5b117ee2c130f5e55afb0dc53728e11d575
Sam Zhou <samlink@126.com>
2024-11-29 10:14:59 +0800

    ch

--- src/html.rs
+++ src/html.rs
@@ -56,7 +56,7 @@ pub async fn user_set(db: web::Data<Pool>, id: Identity) -> HttpResponse {
 pub async fn home(_req: HttpRequest, db: web::Data<Pool>, id: Identity) -> HttpResponse {
     let user = get_user(db, id, "".to_owned()).await;
     if user.name != "" {
-        let html = r2s(|o| productset_html(o));
+        let html = r2s(|o| productset_html(o, user.name));
         HttpResponse::Ok().content_type("text/html").body(html)
     } else {
         goto_login()
